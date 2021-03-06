8d5ddfd19b82b31b29d24e1f033cbb09e5f2a913
Sandeep <sandeepn@bu.edu>
2021-03-06 06:54:07 -0500

    functionality final stages

--- js/script.js
+++ js/script.js
@@ -1,26 +1,145 @@
-let apiData;
+let apiData, categories, selectedCategory, questions = [];
 
-const URL =
-  "https://cdn.contentful.com/spaces/tgp7uzkwkoer/environments/master/entries?access_token=7Mk1mGkO7qdF4JFqDQztUdD9K1p2WlfNP6HLiQnp-Oc&content_type=triviaq";
-$.ajax(URL).then((data) => {
-  console.log(data);
-  apiData = data;
-});
+const $squares = $(".square")
+const $disabledButton = $('#disabled-game')
+const $resetGame = $('#reset-game')
+const $primaryButton = $('#play-game')
+const $categorySection = $('#category-section')
+let timerCount = 30
+let $question = $("#question")
+let $A = $("#a")
+let $B = $("#b")
+let $C = $("#c")
+let $D = $("#d")
+let $answer = $("#answer")
+let $p1score = $("#player1 h4")
+let $p2score = $("#player2 h4")
+let $message = $("#message-board")
 
-$("#single-player").on("click", handleSinglePlayerLinkClick);
-
-function handleSinglePlayerLinkClick(event) {
-  event.preventDefault();
-  redirectSinglePlayer();
+let currentStateOfTrivia = {
+  player1: 0,
+  player2: 0,
+  currentQuestion: {},
+  which: true
 }
 
-function redirectSinglePlayer() {
-  $.ajax({
-    url: "single-player.html",
-    dataType: "html",
-  }).then((response) => {
-    $(".single-player-content").html(response);
-    console.log(apiData.items[0].fields.question);
-    $(".show-question").text(apiData.items[0].fields.question);
+//set the category ids for the categories
+$('#sports').data('myVal', 4);
+$('#entertainment').data('myVal', 1);
+$('#history').data('myVal', 2);
+$('#science').data('myVal', 5);
+$('#geography').data('myVal', 3);
+
+//disable auto slide for bootstrap
+$('.carousel').carousel({
+  interval: false,
+});
+
+//set base URL for All api calls
+const Baseurl = "https://cdn.contentful.com/spaces/tgp7uzkwkoer/environments/master/entries?access_token=7Mk1mGkO7qdF4JFqDQztUdD9K1p2WlfNP6HLiQnp-Oc&content_type="
+
+//get categories
+$.ajax(Baseurl+ 'categories').then(categoryData => {
+  categories = categoryData.items.map(c => {
+    return {
+      category: c.fields.categoryName,
+      id: c.fields.categoryId
+    }
   });
+  console.log(categories)
+});
+
+
+//hide questions and answers content on first screen
+$("#questions-main-content").hide()
+$primaryButton.hide()
+$resetGame.hide()
+
+let highlightClass = 'highlighted-category';
+
+$squares.click(function() {
+  $squares.removeClass(highlightClass)
+  $(this).addClass(highlightClass)
+  selectedCategory = $(this).data("myVal")
+  $primaryButton.show()
+  $disabledButton.hide()
+})
+
+$("#play-game").on("click", playGameButtonClicked);
+
+function playGameButtonClicked(event) {
+  event.preventDefault();
+  renderPlayerHTML();
+}
+
+$resetGame.on("click", function() {
+  location.reload(true)
+})
+
+function renderPlayerHTML() {
+  $.ajax(Baseurl+ `${selectedCategory}`).then(eData => {
+    console.log(eData)
+    questions = eData.items.map(q => {
+      return q.fields
+    })
+    console.log(questions)
+
+    setQuestions(questions);
+
+    $categorySection.hide()
+    $("#trivia-carousel-content").hide()
+    $("#questions-main-content").show()
+    $primaryButton.hide()
+    $resetGame.show()
+  })
+}
+
+function setQuestions(q) {
+  const randomIndex = Math.floor(Math.random()*q.length)
+  const randomQuestion = q[randomIndex]
+  console.log(randomQuestion)
+  $question.text(randomQuestion.question)
+  $A.text(randomQuestion.a)
+  $B.text(randomQuestion.b)
+  $C.text(randomQuestion.c)
+  $D.text(randomQuestion.d)
+  // $answer.text(randomQuestion.answer)
+  
+  $p1score.text(currentStateOfTrivia.player1)
+  $p2score.text(currentStateOfTrivia.player2)  
+
+  console.log(currentStateOfTrivia.player1, currentStateOfTrivia.player2)
+
+  if(currentStateOfTrivia.which) {
+    $message.text(`Player1's Turn`)
+  } else {
+    $message.text(`Player2's Turn`)
+  }
+
+  $("li").off()
+  $("li").on("click", (event) => {
+      chooseAnswer(event, randomQuestion)
+  })
+}
+
+function chooseAnswer(event, question) {
+  if(event.target.innerText === question.answer) {
+    console.log("correct")
+    if(currentStateOfTrivia.which) {
+      currentStateOfTrivia.player1++
+      currentStateOfTrivia.which = !currentStateOfTrivia.which
+    }else {
+      console.log("incorrect")
+        currentStateOfTrivia.player2++
+        currentStateOfTrivia.which = !currentStateOfTrivia.which
+    }
+   
+    setQuestions(questions)
+  } else {
+    
+    setQuestions(questions)
+    currentStateOfTrivia.which = !currentStateOfTrivia.which
+  }
+  
+  setQuestions(questions)
 }
